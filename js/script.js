e016b2513f5a1253bb5aabbc4344b7879890bd56
AndrewKonev <a9139286138@yandex.ru>
2023-05-12 02:26:06 +0700

    add form

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+  var professions = ["Программист", "Преподаватель", "Продавец", "Психолог", "Верстальщик"];
+
+  const autoСomplete = document.querySelector('.profession');
+  autoСomplete.addEventListener('keyup', function () {
+    let text = document.getElementById("profession").value.toLowerCase();
+    let suggestions = document.getElementById("suggestions");
+    suggestions.innerHTML = "";
+    if (text !== "") {
+      var matches = professions.filter(function(profession) {
+        return profession.toLowerCase().startsWith(text);
+      });
+      let list = document.createElement("ul");
+      matches.forEach(function(match) {
+        let item = document.createElement("li");
+        item.textContent = match;
+        item.addEventListener("click", function() {
+          document.getElementById("profession").value = match;
+          suggestions.innerHTML = "";
+        });
+        list.appendChild(item);
+      });
+      suggestions.appendChild(list);
+    }
+  })
+
+
+
